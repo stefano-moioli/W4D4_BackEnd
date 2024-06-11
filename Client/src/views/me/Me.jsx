4ce44179c85c26b878c83c6d2daa23a5ce022268
stefano-moioli <151653556+stefano-moioli@users.noreply.github.com>
2024-06-11 17:37:38 +0200

    updatedMePage

--- Client/src/views/me/Me.jsx
+++ Client/src/views/me/Me.jsx
@@ -5,7 +5,7 @@ export default function Me(){
     return (
     <div className="text-center mt-5">
       <h1>Pagina Autore</h1>
-      <p>Questa è tua pagina</p>
+      <p>Questa è la tua pagina</p>
     </div>
     )
 }

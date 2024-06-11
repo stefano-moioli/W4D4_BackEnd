9673bb9e47bb9ef3519a23be28768aae11b7241b
stefano-moioli <151653556+stefano-moioli@users.noreply.github.com>
2024-06-11 17:42:25 +0200

    fixedMePage

--- Client/src/App.js
+++ Client/src/App.js
@@ -20,7 +20,7 @@ function App() {
         <Route path="/new" element={<NewBlogPost />} />
         <Route path="/auth/login" element={<Login /> } />
         <Route path="/auth/register" element={<Register/> } />
-        <Route path="/me" element={<Me/>} />
+        <Route path="/me" element={<Me />} />
         <Route path="*" element={<Error /> } />
       </Routes>
       <Footer />
